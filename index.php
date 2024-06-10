d70219bdc898e744f202d3f06d3bc60951a24ade
JamilDaiko <161781470+JamilDaiko@users.noreply.github.com>
2024-06-10 19:53:58 -0300

    mais atualizção do home

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <title>Nexus</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -17,20 +21,23 @@
                     <img src="imagens/image-removebg-preview (7) (1).png" alt="" width="120px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon icon-white"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php?param=home">Home</a>
+                            <a class="nav-link active fw-bold" aria-current="page" href="index.php?param=home">HOME</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php?param=contato">Contato</a>
+                            <a class="nav-link active fw-bold" aria-current="page" href="index.php?param=contato">CONTATO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fw-bold" aria-current="page" href="index.php?param=quemsomos">QUEM SOMOS</a>
                         </li>
                     </ul>
                     <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <button class="btn fw-bold" type="submit">Entrar</button>
+                        <button class="btn fw-bold" type="submit">Cadastre</button>
                     </form>
                 </div>
             </div>
@@ -61,8 +68,11 @@
         }
         ?>
     </main>
-    <footer>
+    <footer class="text-center">
+        <h4>&copy; 2024 - Todos os direitos resservados - Desenvolvido por Jamil Jose Daiko Junior</h4>
 
+        <p>Entre em contato pelo e-mail: juniordaiko2005@gmail.com</p>
+        Fone para contato: (44) 98403-5623
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
--- paginas/home.php
+++ paginas/home.php
@@ -1,3 +1,99 @@
-<div class="container">
-    <h1>Home</h1>
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+      aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+      aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+      aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+      aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagens/file (4).jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/Goblin.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/Caminho.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/Nave.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<div class="container my-4">
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Imagem de Exemplo">
+        <div class="card-body">
+          <h5 class="card-title">Título do Card 1</h5>
+          <p class="card-text">Algum texto de exemplo rápido para construir o título do card e preencher o conteúdo do
+            card.</p>
+          <a href="#" class="btn btn-primary">Ir a algum lugar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Imagem de Exemplo">
+        <div class="card-body">
+          <h5 class="card-title">Título do Card 2</h5>
+          <p class="card-text">Algum texto de exemplo rápido para construir o título do card e preencher o conteúdo do
+            card.</p>
+          <a href="#" class="btn btn-primary">Ir a algum lugar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Imagem de Exemplo">
+        <div class="card-body">
+          <h5 class="card-title">Título do Card 3</h5>
+          <p class="card-text">Algum texto de exemplo rápido para construir o título do card e preencher o conteúdo do
+            card.</p>
+          <a href="#" class="btn btn-primary">Ir a algum lugar</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container my-4">
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Imagem de Exemplo">
+        <div class="card-body">
+          <h5 class="card-title">Título do Card 1</h5>
+          <p class="card-text">Algum texto de exemplo rápido para construir o título do card e preencher o conteúdo do
+            card.</p>
+          <a href="#" class="btn btn-primary">Ir a algum lugar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Imagem de Exemplo">
+        <div class="card-body">
+          <h5 class="card-title">Título do Card 2</h5>
+          <p class="card-text">Algum texto de exemplo rápido para construir o título do card e preencher o conteúdo do
+            card.</p>
+          <a href="#" class="btn btn-primary">Ir a algum lugar</a>
+          <a href="#" class="btn btn-primary">Ir a algum lugar</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
